456fb81e95e8357ee742db849891ca9b9e4e8a2a
Ralph Maron Eda <edaralphmaron@gmail.com>
2024-06-03 15:39:42 +0800

    login form ui

--- Auth/AuthForm.cs
+++ Auth/AuthForm.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using TimeSprout.Core.Utils;
 
 namespace TimeSprout
 {
@@ -10,11 +12,55 @@ namespace TimeSprout
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            MainForm mainForm = new MainForm();
 
+        #region DRAGGING_FROM_WITH_LABEL
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HTCAPTION = 0x2;
+        private void titlePanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
+            }
+        }
+
+        #endregion DRAGGING_FROM_WITH_LABEL
+
+
+        private void tbStatus_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Suppress any key presses to make the ComboBox readonly
+            e.Handled = true;
+        }
+
+        private void btnLogin_Click(object sender, System.EventArgs e)
+        {
+            Console.WriteLine($"Username: {tbUsername.Text}, Status: {tbStatus.Text}, Password: {tbPassword.Text}");
+
+            MainForm mainForm = new MainForm();
             mainForm.ShowDialog();
+        }
+
+        private void btnLogin_MouseEnter(object sender, EventArgs e)
+        {
+            btnLogin.BackColor = ColorPalette.AccentLavender;
+        }
+
+        private void btnLogin_MouseLeave(object sender, EventArgs e)
+        {
+            btnLogin.BackColor = ColorPalette.PrimaryPurple;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
---
+++ Core/Utils/ColorPalette.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+
+namespace TimeSprout.Core.Utils
+{
+    internal class ColorPalette
+    {
+        public static readonly Color PrimaryPurple = ColorTranslator.FromHtml("#6A1B9A");
+        public static readonly Color White = ColorTranslator.FromHtml("#FFFFFF");
+        public static readonly Color AccentLavender = ColorTranslator.FromHtml("#D1C4E9");
+    }
+}
