2d9056f84c3fdcc581b738c445ec832c31566f77
Ralph Maron Eda <edaralphmaron@gmail.com>
2024-06-03 15:01:56 +0800

    initial commit

---
+++ Auth/AuthForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TimeSprout
+{
+    public partial class AuthForm : Form
+    {
+        public AuthForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MainForm mainForm = new MainForm();
+
+            mainForm.ShowDialog();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TimeSprout")]
+[assembly: AssemblyDescription("My Project for sy: 2023-2024 as a 3rd Year Computer Engineering Student.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Ralph Maron Eda")]
+[assembly: AssemblyProduct("TimeSprout")]
+[assembly: AssemblyCopyright("Copyright © Ralph Maron Eda 2024. All Rights Reserved.")]
+[assembly: AssemblyTrademark("Ralph Maron Eda")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7421b7a0-0a73-42be-b0b3-e0245a1c3aea")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
