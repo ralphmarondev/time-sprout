68ebf57513b540b3191f7488714aad32fed0e8be
Ralph Maron Eda <edaralphmaron@gmail.com>
2024-06-04 16:59:06 +0800

    time record initial ui

--- Forms/TimeRecord/TimeRecordForm.cs
+++ Forms/TimeRecord/TimeRecordForm.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
+using TimeSprout.Forms.TimeRecord.Model;
 
 namespace TimeSprout.Forms.TimeRecord
 {
@@ -9,6 +11,7 @@ namespace TimeSprout.Forms.TimeRecord
         {
             InitializeComponent();
             SetTextBoxDateOfWeek();
+            populatePanelWithEmployees();
         }
 
         private void dateTimePicker1_ValueChanged(object sender, System.EventArgs e)
@@ -25,5 +28,67 @@ namespace TimeSprout.Forms.TimeRecord
             DateTime selectedDate = dateTimePicker1.Value;
             tbDayOfWeek.Text = selectedDate.ToString("dddd");
         }
+
+        private List<TimeRecordDataClass> employees = new List<TimeRecordDataClass>();
+        void populatePanelWithEmployees()
+        {
+            employees.Clear();
+            employees.Add(
+                new TimeRecordDataClass(
+                date: "2024-06-04",
+                id: "2024-01",
+                employeeName: "Ralph Maron Eda",
+                currentProject: "Earth 2.0",
+                amTimeIn: "8:10",
+                amTimeOut: "12:00",
+                pmTimeIn: "1:20",
+                pmTimeOut: "6:00",
+                otTimeIn: "7:30",
+                otTimeOut: "10:50")
+                );
+            employees.Add(
+                new TimeRecordDataClass(
+                date: "2024-06-04",
+                id: "2024-02",
+                employeeName: "Jackie Ferreras",
+                currentProject: "Earth 2.1",
+                amTimeIn: "8:00",
+                amTimeOut: "12:00",
+                pmTimeIn: "1:00",
+                pmTimeOut: "6:00",
+                otTimeIn: "7:00",
+                otTimeOut: "10:00")
+                );
+
+            List<UserControlTimeRecord> userControls = new List<UserControlTimeRecord>();
+
+            foreach (var employee in employees)
+            {
+                UserControlTimeRecord userControl = new UserControlTimeRecord(
+                    recordDate: employee.RecordDate,
+                    iD: employee.ID,
+                    employeeName: employee.EmployeeName,
+                    currentProject: employee.CurrentProject,
+                    amTimeIn: employee.AmTimeIn,
+                    amTimeOut: employee.AmTimeOut,
+                    pmTimeIn: employee.PmTimeIn,
+                    pmTimeOut: employee.AmTimeOut,
+                    otTimeIn: employee.AmTimeOut,
+                    otTimeOut: employee.AmTimeOut
+                    );
+                userControls.Add(userControl);
+            }
+
+            foreach (var userControl in userControls)
+            {
+                userControl.Dock = DockStyle.Top;
+                userControl.Width = int.MaxValue; // fillMaxWidth()
+                //userControl.BackColor = ColorPalette.AccentLavender;
+
+                employeesPanel.Controls.Add(userControl);
+            }
+
+        }
+
     }
 }
--- Forms/TimeRecord/UserControlTimeRecord.cs
+++ Forms/TimeRecord/UserControlTimeRecord.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace TimeSprout.Forms.TimeRecord
 {
@@ -15,6 +7,32 @@ namespace TimeSprout.Forms.TimeRecord
         public UserControlTimeRecord()
         {
             InitializeComponent();
+        }
+        public string RecordDate { get; set; }
+
+        public string ID { get; set; }
+        public string EmployeeName { get; set; }
+        public string CurrentProject { get; set; }
+        public string AmTimeIn { get; set; }
+        public string AmTimeOut { get; set; }
+        public string PmTimeIn { get; set; }
+        public string PmTimeOut { get; set; }
+        public string OtTimeIn { get; set; }
+        public string OtTimeOut { get; set; }
+
+        public UserControlTimeRecord(string recordDate, string iD, string employeeName, string currentProject, string amTimeIn, string amTimeOut, string pmTimeIn, string pmTimeOut, string otTimeIn, string otTimeOut)
+        {
+            InitializeComponent();
+
+            lblRecordDate.Text = recordDate;
+            lblID.Text = iD;
+            lblName.Text = employeeName;
+            lblAmTimeIn.Text = amTimeIn;
+            lblAmTimeOut.Text = amTimeOut;
+            lblPmTimeIn.Text = pmTimeIn;
+            lblPmTimeOut.Text = pmTimeOut;
+            lblOtTimeIn.Text = otTimeIn;
+            lblOtTimeOut.Text = otTimeOut;
         }
     }
 }
