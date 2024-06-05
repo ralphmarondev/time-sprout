303965785fb2f6419af0f27a75602accf5ebb689
ralphmaron <ralphmaron.dev@gmail.com>
2024-06-05 13:34:59 +0800

    added excel dependencies and exporting all the content of tables in database to an excel file in different sheets

--- Forms/Export/ExportForm.Designer.cs
+++ Forms/Export/ExportForm.Designer.cs
@@ -29,27 +29,93 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbDate = new System.Windows.Forms.TextBox();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Courier New", 16.2F);
-            this.label1.Location = new System.Drawing.Point(217, 193);
+            this.label1.Location = new System.Drawing.Point(191, 206);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(405, 31);
             this.label1.TabIndex = 0;
             this.label1.Text = "EXPORT FROM DB TO EXCEL";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(106)))), ((int)(((byte)(27)))), ((int)(((byte)(154)))));
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.tbDate);
+            this.panel1.Controls.Add(this.btnExport);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 65);
+            this.panel1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(12, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 22);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "EXPORT/HOME";
+            // 
+            // tbDate
+            // 
+            this.tbDate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbDate.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbDate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(106)))), ((int)(((byte)(27)))), ((int)(((byte)(154)))));
+            this.tbDate.Location = new System.Drawing.Point(507, 16);
+            this.tbDate.Name = "tbDate";
+            this.tbDate.Size = new System.Drawing.Size(184, 30);
+            this.tbDate.TabIndex = 2;
+            this.tbDate.Text = "05/06/2024";
+            // 
+            // btnExport
+            // 
+            this.btnExport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExport.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExport.Location = new System.Drawing.Point(697, 12);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(91, 37);
+            this.btnExport.TabIndex = 1;
+            this.btnExport.Text = "EXPORT";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Courier New", 16.2F);
+            this.label2.Location = new System.Drawing.Point(9, 367);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(779, 31);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "EXCEL FILE IS LOCATED ON /BIN/DEBUG/FILE.xlxs";
             // 
             // ExportForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "ExportForm";
             this.Text = "ExportForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -58,5 +124,10 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbDate;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Label label2;
     }
 }
